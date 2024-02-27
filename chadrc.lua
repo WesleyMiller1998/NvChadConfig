b79e045c1b6b12fff79c3aa9e534627db64feaa3
wesley Miller <millerwes1998@gmail.com>
2024-02-26 19:57:52 -0500

    move function to chadrc.lua add GoGet and GoModTidy

--- chadrc.lua
+++ chadrc.lua
@@ -5,6 +5,26 @@ M.ui = { theme = "catppuccin" }
 
 M.plugins = "custom.plugins"
 
-require("custom.configs.open_nvim")
+local function open_nvim_tree()
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+local function go_mod_tidy()
+  vim.cmd [[ ! go mod tidy ]]
+  vim.cmd [[ LspRestart ]]
+end
+
+vim.api.nvim_create_user_command("GoModTidy", go_mod_tidy, {})
+
+local function go_get(opts)
+  vim.cmd([[ ! go get ]] .. opts.fargs[1])
+  if #vim.lsp.get_active_clients() > 0 then
+    vim.cmd [[ LspRestart ]]
+  end
+end
+
+vim.api.nvim_create_user_command("GoGet", go_get, { nargs = 1 })
 
 return M
